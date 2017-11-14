75f91927632f3b35e8ee9d29a63594e0ce937cca
Frederic GEORGES <frederic.georges@telecomnancy.net>
2017-11-14 15:17:42 +0100

    index updt

--- src/main/java/hackathon/LuceneIndex.java
+++ src/main/java/hackathon/LuceneIndex.java
@@ -2,19 +2,11 @@
 package hackathon;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
-import java.lang.reflect.Field;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
+import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Date;
 
@@ -26,7 +18,6 @@ import org.apache.lucene.document.LongPoint;
 import org.apache.lucene.document.StoredField;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
@@ -34,146 +25,220 @@ import org.apache.lucene.index.IndexableField;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.store.LockObtainFailedException;
-import org.apache.lucene.store.SimpleFSDirectory;
-import org.apache.lucene.util.Version;
 
 public class LuceneIndex {
-	private LuceneIndex() {}
-	
-	  /** Index all text files under a directory. */
-	  public static void main(String[] args) {
-	    String usage = "java org.apache.lucene.demo.IndexFiles"
-	                 + " [-index INDEX_PATH] [-docs DOCS_PATH] [-update]\n\n"
-	                 + "This indexes the documents in DOCS_PATH, creating a Lucene index"
-	                 + "in INDEX_PATH that can be searched with SearchFiles";
-	    String indexPath = "index";
-	    String docsPath = null;
-	    boolean create = true;
-	    for(int i=0;i<args.length;i++) {
-	      if ("-index".equals(args[i])) {
-	        indexPath = args[i+1];
-	        i++;
-      } else if ("-docs".equals(args[i])) {
-	        docsPath = args[i+1];
-	        i++;
-	      } else if ("-update".equals(args[i])) {
-	        create = false;
-	      }
-	    }
-	
-	    if (docsPath == null) {
-	      System.err.println("Usage: " + usage);
-	      System.exit(1);
-	    }
-	
-	    final Path docDir = Paths.get(docsPath);
-	    if (!Files.isReadable(docDir)) {
-	      System.out.println("Document directory '" +docDir.toAbsolutePath()+ "' does not exist or is not readable, please check the path");
-	      System.exit(1);
-	    }
-	    
-	    Date start = new Date();
-	    try {
-	      System.out.println("Indexing to directory '" + indexPath + "'...");
-	
-	     Directory dir = FSDirectory.open(Paths.get(indexPath));
-	      Analyzer analyzer = new StandardAnalyzer();
-	      IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
-	
-	      if (create) {
-	        // Create a new index in the directory, removing any
-	        // previously indexed documents:
-	        iwc.setOpenMode(OpenMode.CREATE);
-	      } else {
-	        // Add new documents to an existing index:
-	        iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
-	}
-	
-	      IndexWriter writer = new IndexWriter(dir, iwc);
-	      indexDocs(writer, docDir);
-	
-	      writer.close();
-	
-	      Date end = new Date();
-	      System.out.println(end.getTime() - start.getTime() + " total milliseconds");
-	
-	    } catch (IOException e) {
-	      System.out.println(" caught a " + e.getClass() +
-	       "\n with message: " + e.getMessage());
-	    }
-	}
-	  
-	  static void indexDocs(final IndexWriter writer, Path path) throws IOException {
-		      if (Files.isDirectory(path)) {
-		        Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
-		          @Override
-		          public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-		            try {
-		              indexDoc(writer, file, attrs.lastModifiedTime().toMillis());
-		            } catch (IOException ignore) {
-		              // don't index files that can't be read.
-		            }
-		            return FileVisitResult.CONTINUE;
-		          }
-		        });
-		      } else {
-		        indexDoc(writer, path, Files.getLastModifiedTime(path).toMillis());
-		      }
-		    }
-	  
-	  /** Indexes a single document */
-	    static void indexDoc(IndexWriter writer, Path file, long lastModified) throws IOException {
-	    	System.out.println("file = "+ file);
-	      try (InputStream stream = Files.newInputStream(file)) {
+    private LuceneIndex() {
+    }
 
-	    	  System.out.println("stream = " + stream);
-	      	InputStreamReader ipsr= new InputStreamReader(stream, StandardCharsets.UTF_8);
-	      	System.out.println("ipsr = "+ ipsr.getEncoding());
-	      	BufferedReader br = new BufferedReader(ipsr);
-	        // make a new, empty document
-	        Document doc = new Document();
-	        String line;
-	        IndexableField pathField = new StringField("path", file.toString(), Store.YES);
-	        doc.add(pathField);	  
-	        doc.add(new LongPoint("modified", lastModified));
+    /**
+     * Index all text files under a directory.
+     */
+    public static void main(String[] args) {
+        String usage = "java org.apache.lucene.demo.IndexFiles"
+                + " [-index INDEX_PATH] [-docs DOCS_PATH] [-update]\n\n"
+                + "This indexes the documents in DOCS_PATH, creating a Lucene index"
+                + "in INDEX_PATH that can be searched with SearchFiles";
+        String indexPath = "index";
+        String docsPath = null;
+        boolean create = true;
+        for (int i = 0; i < args.length; i++) {
+            if ("-index".equals(args[i])) {
+                indexPath = args[i + 1];
+                i++;
+            } else if ("-docs".equals(args[i])) {
+                docsPath = args[i + 1];
+                i++;
+            } else if ("-update".equals(args[i])) {
+                create = false;
+            }
+        }
 
-	        System.out.println("buff = " + br.readLine());
-	        while((line = br.readLine()) != null)
-	    	 {
+        if (docsPath == null) {
+            System.err.println("Usage: " + usage);
+            System.exit(1);
+        }
 
-	        	System.out.print("line = "+ line +"\n");
-		    		 String[] tmp = line.split(";");
-		    		 System.out.println("tmp = "+ tmp[4]);
-					  String MNCP_NAME =tmp[4];
-					  String PRSN_INTERNALID = tmp[0];
-					  String PRSN_SEX = tmp[2];
-					  String VILLE_MEDECIN = tmp[6];
-					  String PAYS_MEDECIN = tmp[7];
-					  String PRSN_BIRTHDATE = tmp[1];
-					  doc.add(new TextField("MNCP_NAME",MNCP_NAME, Store.YES));
-					  doc.add(new StoredField("PRSN_INTERNALID",PRSN_INTERNALID));
-					  doc.add(new StoredField("PRSN_SEX",PRSN_SEX));
-					  doc.add(new StoredField("VILLE_MEDECIN", VILLE_MEDECIN));
-					  doc.add(new StoredField("PAYS_MEDECIN",PAYS_MEDECIN));
-					  doc.add(new StoredField("PRSN_BIRTHDATE",PRSN_BIRTHDATE));
-	    			  writer.addDocument(doc);
+        final Path docDir = Paths.get(docsPath);
+        if (!Files.isReadable(docDir)) {
+            System.out.println("Document directory '" + docDir.toAbsolutePath() + "' does not exist or is not readable, please check the path");
+            System.exit(1);
+        }
 
-	    		  	  doc = new Document();
-		    	 
-	    	 }
-	        
-	        if (writer.getConfig().getOpenMode() == OpenMode.CREATE) {
-	          // New index, so we just add the document (no old document can be there):
-	          System.out.println("adding " + file);
-	          writer.addDocument(doc);
-	        } else {
-	          // Existing index (an old copy of this document may have been indexed) so 
-	          // we use updateDocument instead to replace the old one matching the exact 
-	          // path, if present:
-	          System.out.println("updating " + file);
-	          writer.updateDocument(new Term("path", file.toString()), doc);
-	        }
-	      }
-	    }
+        Date start = new Date();
+        try {
+            System.out.println("Indexing to directory '" + indexPath + "'...");
+
+            Directory dir = FSDirectory.open(Paths.get(indexPath));
+            Analyzer analyzer = new StandardAnalyzer();
+            IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+
+            if (create) {
+                // Create a new index in the directory, removing any
+                // previously indexed documents:
+                iwc.setOpenMode(OpenMode.CREATE);
+            } else {
+                // Add new documents to an existing index:
+                iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
+            }
+
+            IndexWriter writer = new IndexWriter(dir, iwc);
+            indexDocsByMNCP(writer, docDir);
+
+            writer.close();
+
+            Date end = new Date();
+            System.out.println(end.getTime() - start.getTime() + " total milliseconds");
+
+        } catch (IOException e) {
+            System.out.println(" caught a " + e.getClass() +
+                    "\n with message: " + e.getMessage());
+        }
+    }
+
+    static void indexDocsByMNCP(final IndexWriter writer, Path path) throws IOException {
+        if (Files.isDirectory(path)) {
+            Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    try {
+                        indexDocByMNCP(writer, file, attrs.lastModifiedTime().toMillis());
+                    } catch (IOException ignore) {
+                        // don't index files that can't be read.
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } else {
+            indexDocByMNCP(writer, path, Files.getLastModifiedTime(path).toMillis());
+        }
+    }
+
+    /**
+     * Indexes a single document
+     */
+    static void indexDocByMNCP(IndexWriter writer, Path file, long lastModified) throws IOException {
+        System.out.println("file = " + file);
+        try (InputStream stream = Files.newInputStream(file)) {
+
+            System.out.println("stream = " + stream);
+            InputStreamReader ipsr = new InputStreamReader(stream, StandardCharsets.UTF_8);
+            System.out.println("ipsr = " + ipsr.getEncoding());
+            BufferedReader br = new BufferedReader(ipsr);
+            // make a new, empty document
+            Document doc = new Document();
+            String line;
+            IndexableField pathField = new StringField("path", file.toString(), Store.YES);
+            doc.add(pathField);
+            doc.add(new LongPoint("modified", lastModified));
+
+            System.out.println("buff = " + br.readLine());
+            while ((line = br.readLine()) != null) {
+
+                System.out.print("line = " + line + "\n");
+                String[] tmp = line.split(";");
+                System.out.println("tmp = " + tmp[4]);
+                String MNCP_NAME = tmp[4];
+                String PRSN_INTERNALID = tmp[0];
+                String PRSN_SEX = tmp[2];
+                String VILLE_MEDECIN = tmp[6];
+                String PAYS_MEDECIN = tmp[7];
+                String PRSN_BIRTHDATE = tmp[1];
+                doc.add(new TextField("MNCP_NAME", MNCP_NAME, Store.YES));
+                doc.add(new StoredField("PRSN_INTERNALID", PRSN_INTERNALID));
+                doc.add(new StoredField("PRSN_SEX", PRSN_SEX));
+                doc.add(new StoredField("VILLE_MEDECIN", VILLE_MEDECIN));
+                doc.add(new StoredField("PAYS_MEDECIN", PAYS_MEDECIN));
+                doc.add(new StoredField("PRSN_BIRTHDATE", PRSN_BIRTHDATE));
+                writer.addDocument(doc);
+
+                doc = new Document();
+
+            }
+
+            writerUpdateUpdate(writer, file, doc);
+        }
+    }
+
+    private static void writerUpdateUpdate(IndexWriter writer, Watchable file, Iterable<IndexableField> doc) throws IOException {
+        writerUpdate(writer, file, doc);
+    }
+
+    private static void writerUpdate(IndexWriter writer, Watchable file, Iterable<IndexableField> doc) throws IOException {
+        if (writer.getConfig().getOpenMode() == OpenMode.CREATE) {
+            // New index, so we just add the document (no old document can be there):
+            System.out.println("adding " + file);
+            writer.addDocument(doc);
+        } else {
+            // Existing index (an old copy of this document may have been indexed) so
+            // we use updateDocument instead to replace the old one matching the exact
+            // path, if present:
+            System.out.println("updating " + file);
+            writer.updateDocument(new Term("path", file.toString()), doc);
+        }
+    }
+
+
+    static void indexDocsByMED(final IndexWriter writer, Path path) throws IOException {
+        if (Files.isDirectory(path)) {
+            Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    try {
+                        indexDocByMED(writer, file, attrs.lastModifiedTime().toMillis());
+                    } catch (IOException ignore) {
+                        // don't index files that can't be read.
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } else {
+            indexDocByMED(writer, path, Files.getLastModifiedTime(path).toMillis());
+        }
+    }
+
+    /**
+     * Indexes a single document
+     */
+    static void indexDocByMED(IndexWriter writer, Path file, long lastModified) throws IOException {
+        System.out.println("file = " + file);
+        try (InputStream stream = Files.newInputStream(file)) {
+
+            System.out.println("stream = " + stream);
+            InputStreamReader ipsr = new InputStreamReader(stream, StandardCharsets.UTF_8);
+            System.out.println("ipsr = " + ipsr.getEncoding());
+            BufferedReader br = new BufferedReader(ipsr);
+            // make a new, empty document
+            Document doc = new Document();
+            String line;
+            IndexableField pathField = new StringField("path", file.toString(), Store.YES);
+            doc.add(pathField);
+            doc.add(new LongPoint("modified", lastModified));
+
+            System.out.println("buff = " + br.readLine());
+            while ((line = br.readLine()) != null) {
+
+                System.out.print("line = " + line + "\n");
+                String[] tmp = line.split(";");
+                System.out.println("tmp = " + tmp[4]);
+                String MNCP_NAME = tmp[4];
+                String PRSN_INTERNALID = tmp[0];
+                String PRSN_SEX = tmp[2];
+                String VILLE_MEDECIN = tmp[6];
+                String PAYS_MEDECIN = tmp[7];
+                String PRSN_BIRTHDATE = tmp[1];
+                doc.add(new StoredField("MNCP_NAME", MNCP_NAME));
+                doc.add(new StoredField("PRSN_INTERNALID", PRSN_INTERNALID));
+                doc.add(new StoredField("PRSN_SEX", PRSN_SEX));
+                doc.add(new TextField("VILLE_MEDECIN", VILLE_MEDECIN, Store.YES));
+                doc.add(new StoredField("PAYS_MEDECIN", PAYS_MEDECIN));
+                doc.add(new StoredField("PRSN_BIRTHDATE", PRSN_BIRTHDATE));
+                writer.addDocument(doc);
+
+                doc = new Document();
+
+            }
+
+            writerUpdate(writer, file, doc);
+        }
+    }
 }
--- src/main/java/hackathon/SearchIndex.java
+++ src/main/java/hackathon/SearchIndex.java
@@ -26,250 +26,65 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
 public class SearchIndex {
-	
-	/*private SearchIndex() {}
-	
-  *//** Simple command-line based search demo. 
- * @throws IOException *//*
-	  public static void main(String[] args) throws Exception {
-	    String usage =
-	      "Usage:\tjava org.apache.lucene.demo.SearchFiles [-index dir] [-field f] [-repeat n] [-queries file] [-query string] [-raw] [-paging hitsPerPage]\n\nSee http://lucene.apache.org/core/4_1_0/demo/ for details.";
-	    if (args.length > 0 && ("-h".equals(args[0]) || "-help".equals(args[0]))) {
-	      System.out.println(usage);
-	      System.exit(0);
-	    }
-	
-	    String index = "index";
-	    String field = "contents";
-	    String queries = null;
-	    int repeat = 0;
-	    boolean raw = false;
-	    String queryString = null;
-	    int hitsPerPage = 10;
-	    
-	    for(int i = 0;i < args.length;i++) {
-	      if ("-index".equals(args[i])) {
-	        index = args[i+1];
-	        i++;
-	      } else if ("-field".equals(args[i])) {
-	        field = args[i+1];
-	        i++;
-	      } else if ("-queries".equals(args[i])) {
-	        queries = args[i+1];
-	        i++;
-	      } else if ("-query".equals(args[i])) {
-	        queryString = args[i+1];
-	        i++;
-	      } else if ("-repeat".equals(args[i])) {
-	        repeat = Integer.parseInt(args[i+1]);
-	        i++;
-	      } else if ("-raw".equals(args[i])) {
-	        raw = true;
-	      } else if ("-paging".equals(args[i])) {
-	        hitsPerPage = Integer.parseInt(args[i+1]);
-	        if (hitsPerPage <= 0) {
-	          System.err.println("There must be at least 1 hit per page.");
-	          System.exit(1);
-	        }
-	        i++;
-	      }
-	    }
-	    
-	    IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(index)));
-	    IndexSearcher searcher = new IndexSearcher(reader);
-	    Analyzer analyzer = new StandardAnalyzer();
-	
-	    BufferedReader in = null;
-	    if (queries != null) {
-	      in = Files.newBufferedReader(Paths.get(queries), StandardCharsets.UTF_8);
-	    } else {
-	      in = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
-	    }
-	    QueryParser parser = new QueryParser(field, analyzer);
-	    while (true) {
-	      if (queries == null && queryString == null) {                        // prompt the user
-	        System.out.println("Enter query: ");
-	      }
-	
-	      String line = queryString != null ? queryString : in.readLine();
-	
-	      if (line == null || line.length() == -1) {
-	        break;
-	      }
-	
-	      line = line.trim();
-	      if (line.length() == 0) {
-	        break;
-	      }
-	      
-	      Query query = parser.parse(line);
-	      System.out.println("Searching for: " + query.toString(field));
-	            
-	      if (repeat > 0) {                           // repeat & time as benchmark
-	        Date start = new Date();
-	        for (int i = 0; i < repeat; i++) {
-	          searcher.search(query, 100);
-	        }
-	        Date end = new Date();
-	        System.out.println("Time: "+(end.getTime()-start.getTime())+"ms");
-	      }
-	
-	      doPagingSearch(in, searcher, query, hitsPerPage, raw, queries == null && queryString == null);
-	
-	      if (queryString != null) {
-	        break;
-	      }
-	    }
-	    reader.close();
-	  }
 
-	  public static void doPagingSearch(BufferedReader in, IndexSearcher searcher, Query query, 
-	                                     int hitsPerPage, boolean raw, boolean interactive) throws IOException {
-	 
-	    // Collect enough docs to show 5 pages
-	    TopDocs results = searcher.search(query, 5 * hitsPerPage);
-	    ScoreDoc[] hits = results.scoreDocs;
-	    
-	    int numTotalHits = Math.toIntExact(results.totalHits);
-	    System.out.println(numTotalHits + " total matching documents");
-	
-	    int start = 0;
-	    int end = Math.min(numTotalHits, hitsPerPage);
-	        
-	    while (true) {
-	      if (end > hits.length) {
-	        System.out.println("Only results 1 - " + hits.length +" of " + numTotalHits + " total matching documents collected.");
-	        System.out.println("Collect more (y/n) ?");
-	        String line = in.readLine();
-	        if (line.length() == 0 || line.charAt(0) == 'n') {
-	          break;
-	        }
-	
-	        hits = searcher.search(query, numTotalHits).scoreDocs;
-	      }
-	      
-	      end = Math.min(hits.length, start + hitsPerPage);
-	      
-	      for (int i = start; i < end; i++) {
-	        if (raw) {                              // output raw format
-	          System.out.println("doc="+hits[i].doc+" score="+hits[i].score);
-	          continue;
-	        }
-	
-	        Document doc = searcher.doc(hits[i].doc);
-	        String path = doc.get("path");
-	        if (path != null) {
-	          System.out.println((i+1) + ". " + path);
-	          String title = doc.get("title");
-	          if (title != null) {
-	            System.out.println("   Title: " + doc.get("title"));
-	          }
-	        } else {
-	          System.out.println((i+1) + ". " + "No path for this document");
-	        }
-	                  
-	      }
-	
-	      if (!interactive || end == 0) {
-	        break;
-	      }
-	
-	      if (numTotalHits >= end) {
-	        boolean quit = false;
-	        while (true) {
-	          System.out.print("Press ");
-	          if (start - hitsPerPage >= 0) {
-	            System.out.print("(p)revious page, ");  
-	          }
-	          if (start + hitsPerPage < numTotalHits) {
-	            System.out.print("(n)ext page, ");
-	          }
-	          System.out.println("(q)uit or enter number to jump to a page.");
-	          
-	          String line = in.readLine();
-	          if (line.length() == 0 || line.charAt(0)=='q') {
-	            quit = true;
-	            break;
-	          }
-	          if (line.charAt(0) == 'p') {
-	            start = Math.max(0, start - hitsPerPage);
-	            break;
-	          } else if (line.charAt(0) == 'n') {
-	            if (start + hitsPerPage < numTotalHits) {
-	              start+=hitsPerPage;
-	            }
-	            break;
-	          } else {
-	            int page = Integer.parseInt(line);
-	            if ((page - 1) * hitsPerPage < numTotalHits) {
-	              start = (page - 1) * hitsPerPage;
-	              break;
-	            } else {
-	              System.out.println("No such page");
-	            }
-	          }
-	        }
-	        if (quit) break;
-	        end = Math.min(numTotalHits, start + hitsPerPage);
-	      }
-	   }
-	  }*/
-	public static ArrayList<String> SearchIndex(String path, String search) throws IOException{
-		System.out.println("Path = "+ path);
-		StandardAnalyzer analyzer = new StandardAnalyzer();
-		Directory index = FSDirectory.open(Paths.get(path));
-		System.out.println("length = "+ index.listAll().toString().length());
-		ArrayList<String> res = new ArrayList<String>();
-		String id = "";
-		String sex = "";
-		String date_birth = null;
-		String ville_doctor = "";
-		String pays_doctor = "";
-		 //q est la recherche
-		Query q = null;
-		try {
-			q = new QueryParser("MNCP_NAME", analyzer).parse(search);
-			
-		} catch (org.apache.lucene.queryparser.classic.ParseException e) {
-			e.printStackTrace();
-		}
-		int hitsPerPage = 100;
-		IndexReader reader = DirectoryReader.open(index);
-		IndexSearcher searcher = new IndexSearcher(reader);
-		TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage);
-		searcher.search(q, collector);
-		ScoreDoc[] hits = collector.topDocs().scoreDocs;
-		// =============== Display  =============================
-		System.out.println("Found " + hits.length + " hits.");
-		for (int i = 0; i < hits.length; ++i) {
-			int docId = hits[i].doc;
-			Document d = searcher.doc(docId);
-			System.out.println("document = " + d.getFields());
-			id = (String) d.get("PRSN_INTERNALID");
-			sex = (String) d.get("PRSN_SEX");
-			date_birth = (String) d.get("PRSN_BIRTHDATE");
-			ville_doctor = (String) d.get("VILLE_MEDECIN");
-			pays_doctor = (String) d.get("PAYS_MEDECIN");
-			System.out.println("id = " + id);
-			System.out.println("sex = " + sex);
-			System.out.println("date_birth = " + date_birth);
-			System.out.println("ville_doctor = " + ville_doctor);
-			System.out.println("pays_doctor = " + pays_doctor);
-			res.add(id);
-			res.add(sex);
-			res.add(date_birth);
-			res.add(ville_doctor);
-			res.add(pays_doctor);
-		}
-		
-		reader.close();
-		System.out.println("res = "+ res.get(0));
-		System.out.println("res = "+ res.get(1));
-		System.out.println("res = "+ res.get(5));
-		return res;
-	}
-	public static void main(String[] args) throws Exception {
-	    SearchIndex(args[0], args[1]);
-	}
+
+    public static ArrayList<String> SearchIndex(String path, String search) throws IOException {
+        System.out.println("Path = " + path);
+        StandardAnalyzer analyzer = new StandardAnalyzer();
+        Directory index = FSDirectory.open(Paths.get(path));
+        System.out.println("length = " + index.listAll().toString().length());
+        ArrayList<String> res = new ArrayList<String>();
+        String id = "";
+        String sex = "";
+        String date_birth = null;
+        String ville_doctor = "";
+        String pays_doctor = "";
+        //q est la recherche
+        Query q = null;
+        try {
+            q = new QueryParser("MNCP_NAME", analyzer).parse(search);
+
+        } catch (org.apache.lucene.queryparser.classic.ParseException e) {
+            e.printStackTrace();
+        }
+        int hitsPerPage = 100;
+        IndexReader reader = DirectoryReader.open(index);
+        IndexSearcher searcher = new IndexSearcher(reader);
+        TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage);
+        searcher.search(q, collector);
+        ScoreDoc[] hits = collector.topDocs().scoreDocs;
+        // =============== Display  =============================
+        System.out.println("Found " + hits.length + " hits.");
+        for (int i = 0; i < hits.length; ++i) {
+            int docId = hits[i].doc;
+            Document d = searcher.doc(docId);
+            System.out.println("document = " + d.getFields());
+            id = d.get("PRSN_INTERNALID");
+            sex = d.get("PRSN_SEX");
+            date_birth = d.get("PRSN_BIRTHDATE");
+            ville_doctor = d.get("VILLE_MEDECIN");
+            pays_doctor = d.get("PAYS_MEDECIN");
+            System.out.println("id = " + id);
+            System.out.println("sex = " + sex);
+            System.out.println("date_birth = " + date_birth);
+            System.out.println("ville_doctor = " + ville_doctor);
+            System.out.println("pays_doctor = " + pays_doctor);
+            res.add(id);
+            res.add(sex);
+            res.add(date_birth);
+            res.add(ville_doctor);
+            res.add(pays_doctor);
+        }
+
+        reader.close();
+        System.out.println("res = " + res.get(0));
+        System.out.println("res = " + res.get(1));
+        System.out.println("res = " + res.get(5));
+        return res;
+    }
+
+    public static void main(String[] args) throws Exception {
+        SearchIndex(args[0], args[1]);
+    }
 
 }
