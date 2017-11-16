96e34db88d6cacef1c3c54c400dafe6590bac10c
cpnu <constance.pineau@telecomnancy.eu>
2017-11-16 15:05:34 +0100

    nettoyage

--- src/main/java/hackathon/SearchIndex.java
+++ src/main/java/hackathon/SearchIndex.java
@@ -24,6 +24,43 @@ import org.json.*;
 
 public class SearchIndex {
 
+	public static JSONObject generateGraphicJSON(String path, String cityparam) throws IOException{
+        ArrayList<ArrayList<String>> res = SearchIndex(path, cityparam);
+        HashMap<String, Integer> map = new HashMap<>();
+        int nbConsultations = 0,
+            nbConsultationsIgn =0;
+        float pctEntry = 0;
+        for(ArrayList<String> line : res){
+            nbConsultations++;
+            Integer count = map.get(line.get(4));
+            if (count == null) {
+                map.put(line.get(4), 1);
+            }
+            else {
+                map.put(line.get(4), count + 1);
+            }
+        }
+        JSONObject mncp_medJSON = new JSONObject();
+        mncp_medJSON.put("patientCityName",cityparam);
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            pctEntry=(float)entry.getValue()/(float)nbConsultations;
+            if(pctEntry>0.01f) {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("medicalCityName", entry.getKey());
+                jsonObject.put("nb", entry.getValue());
+                mncp_medJSON.accumulate("medicalCity", jsonObject);
+            }
+            else nbConsultationsIgn+=entry.getValue();
+        }
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("medicalCityName", "Autres");
+        jsonObject.put("nb", nbConsultationsIgn);
+        mncp_medJSON.accumulate("medicalCity", jsonObject);
+
+
+        return mncp_medJSON;
+    }
+
 
     public static void generateMapJSON(String path)throws IOException{
         FileReader f = new FileReader(path+"//villes.txt");
@@ -58,31 +95,17 @@ public class SearchIndex {
             else {
                 map.put(temp, count + 1);
             }
-            //System.out.println("nb patients pour age: "+line.get(6)+", sex: "+line.get(1)+" = "+count);
         }
         JSONObject mapJSON = new JSONObject();
         mapJSON.put("patientCityName",cityparam);
         for(Map.Entry<ArrayList<String>, Integer> entry : map.entrySet()){
             JSONObject jsonObject = new JSONObject();
-            //System.out.println("age"+ entry.getKey()[0]);
-            //System.out.println("sex"+ entry.getKey()[1]);
             jsonObject.put("age", entry.getKey().get(1));
             jsonObject.put("sex", entry.getKey().get(0));
             jsonObject.put("nb", entry.getValue());
             mapJSON.accumulate("medicalCity", jsonObject);
         }
         return mapJSON;
-
-//        File f = new File("mapJSON_"+cityparam+".json");
-//        if(f.exists())
-//            f.delete();
-
-//        FileWriter jsonOutput = new FileWriter(f);
-//
-//        jsonOutput.write(mapJSON.toString(4));
-//
-//        jsonOutput.close();
-//        System.out.println(mncp_medJSON.toString(2));
     }
 
 
@@ -122,7 +145,6 @@ public class SearchIndex {
                 map.put(line.get(4), count + 1);
             }
         }
-        //System.out.println(nbConsultations);
         JSONObject mncp_medJSON = new JSONObject();
         mncp_medJSON.put("patientCityName",cityparam);
         for(Map.Entry<String, Integer> entry : map.entrySet()){
@@ -143,42 +165,12 @@ public class SearchIndex {
 
 
         return mncp_medJSON;
-//        File f = new File("donutJSON_"+cityparam+".json");
-//        if(f.exists())
-//            f.delete();
-//
-//        FileWriter jsonOutput = new FileWriter(f);
-//
-//        jsonOutput.write(mncp_medJSON.toString(4));
-//
-//        jsonOutput.close();
-//        System.out.println(mncp_medJSON.toString(2));
     }
 
 
-	public static int[] nbPatientsparAge(String path, String cityparam) throws IOException{
-		int[] tabAges = new int[10];
-		for(int i=0; i<tabAges.length-1; i++){
-			tabAges[i] = getNbPatients(path,cityparam,"1",String.valueOf(i*10),String.valueOf(i*10+9))
-					+getNbPatients(path,cityparam,"2",String.valueOf(i*10),String.valueOf(i*10+9));
-		}
-		tabAges[9]=getNbPatients(path,cityparam,"1",String.valueOf(90),String.valueOf(110))
-				+getNbPatients(path,cityparam,"2",String.valueOf(90),String.valueOf(110));
-		return tabAges;
-	}
 
-
-	public static int getNbPatients(String path, String cityparam, String sexparam, String agemin, String agemax) throws IOException{
-		return SearchIndex( path,  cityparam,  sexparam,  agemin,  agemax ).size();
-	}
-
-    public static ArrayList<ArrayList<String>> SearchIndex(String path, String cityparam, String sexparam, String agemin, String agemax ) throws IOException {
-    	ArrayList<ArrayList<String>> res = new ArrayList<ArrayList<String>>();
-    	for(int i=Integer.parseInt(agemin); i<Integer.parseInt(agemax); i++)
-    		res.addAll(SearchIndex(path,cityparam,sexparam,String.valueOf(i)));
-    	//System.out.println(res);
-    	return res;
-    }
+	
+    
     public static ArrayList<ArrayList<String>> SearchIndex(String path, String cityparam ) throws IOException {
         //System.out.println("Path = " + path);
         StandardAnalyzer analyzer = new StandardAnalyzer();
@@ -256,198 +248,9 @@ public class SearchIndex {
         return res2;
     }
 
-    public static ArrayList<ArrayList<String>> SearchIndex(String path, String cityparam, String sexparam, String ageparam ) throws IOException {
-        //System.out.println("Path = " + path);
-        StandardAnalyzer analyzer = new StandardAnalyzer();
-        Directory index = FSDirectory.open(Paths.get(path));
-        //System.out.println("length = " + index.listAll().toString().length());
-        ArrayList<ArrayList<String>> res2 = new ArrayList<ArrayList<String>>();
-        
-        String id = "";
-        String sex = "";
-        String date_birth = null;
-        String city_doctor = "";
-        String pays_doctor = "";
-        String dossier = null;
-        //q est la recherche
-        Query q = null;
-        try {
-        	String[] listField = {"MNCP_NAME", "PRSN_AGE", "PRSN_SEX"};
-        	String[] listPath = {cityparam,ageparam,sexparam};
-        	BooleanClause.Occur[] flags = {BooleanClause.Occur.MUST,BooleanClause.Occur.MUST, BooleanClause.Occur.MUST};
-            q = new MultiFieldQueryParser(listField, analyzer).parse(listPath,listField,flags, analyzer);
-        
-           
-
-        } catch (org.apache.lucene.queryparser.classic.ParseException e) {
-            e.printStackTrace();
-        }
-        int hitsPerPage = 1000000;
-        IndexReader reader = DirectoryReader.open(index);
-		
-//System.out.println("reader =" + reader.numDocs());
-        IndexSearcher searcher = new IndexSearcher(reader);
-        //System.out.println("searcher =" + searcher.count(q));
-        TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage);
-        //System.out.println("collector = " + collector.toString());
-        searcher.search(q, collector);
-        ScoreDoc[] hits = collector.topDocs().scoreDocs;
-        // =============== Display  =============================
-        //System.out.println("Found " + hits.length + " hits.");
-        for (int i = 0; i < hits.length; ++i) {
-        	ArrayList<String> res = new ArrayList<String>();
-            int docId = hits[i].doc;
-            Document d = searcher.doc(docId);
-            //System.out.println("document = " + d.getFields());
-            id = d.get("PRSN_INTERNALID");
-            sex = d.get("PRSN_SEX");
-            date_birth = d.get("PRSN_BIRTHDATE");
-            city_doctor = d.get("VILLE_MEDECIN");
-            pays_doctor = d.get("PAYS_MEDECIN");
-            dossier = d.get("DATE_DOSSIER");
-            /*
-            System.out.println("id = " + id);
-            System.out.println("sex = " + sex);
-            System.out.println("date_birth = " + date_birth);
-            System.out.println("ville_doctor = " + ville_doctor);
-            System.out.println("pays_doctor = " + pays_doctor);
-            System.out.println("dossier = "+ dossier);
-            */
-            res.add(id);
-            res.add(sex);
-            res.add(date_birth);
-            res.add(dossier);
-            res.add(city_doctor);
-            res.add(pays_doctor);
-            
-            //System.out.println("res = " + res);
-            res2.add(i, res);
-        }
-
-        reader.close();
-        
-       
-        
-        return res2;
-    }
-    /*
-    public static int[] NbAgeVille (String path, String ville) throws IOException, ParseException{
-    	ArrayList<ArrayList<String>> liste = new ArrayList<ArrayList<String>>();
-    	liste = SearchIndex(path, ville);
-    	int[] age = new int[9];
-    	int size = liste.size();
-    	int cpt=0;
-    	for (int i=0;i<size; i++){
-    		if ( liste.get(i).get(2).length() == 10){
-    			//System.out.println("birth = "+ liste.get(i).get(2) + ", = " + i + " ligne = " + liste.get(i));
-        		int birth = Integer.parseInt(liste.get(i).get(2).substring(0, 4));
-        		//System.out.println("birth ="+ birth);
-        		int dossier = Integer.parseInt(liste.get(i).get(3).substring(0, 4));
-        		//System.out.println("dossier ="+ dossier);
-        		int difference = dossier-birth;
-        		
-        		if (difference<=10){
-        			age[0]+=1;
-        			//break;
-        		}
-        		if (10<difference && difference<=20){
-        			age[1]+=1;
-        			
-        		}
-        		if (20<difference && difference<=30){
-        			age[2]+=1;
-        			
-        		}
-        		if (30<difference && difference<=40){
-        			age[3]+=1;
-        			
-        		}
-        		if (40<difference && difference<=50){
-        			age[4]+=1;
-        			
-        		}
-        		if (50<difference && difference<=60){
-        			age[5]+=1;
-        			
-        		}
-        		if (60<difference && difference<=70){
-        			age[6]+=1;
-        			
-        		}
-        		if (70<difference && difference<=80){
-        			age[7]+=1;
-        			
-        		}
-        		if (80<difference){
-        			age[8]+=1;
-        			
-        		}
-    		}
-    		else {
-    			cpt +=1;
-    		}
-    		
-    	}
-    	System.out.println("age ="+ age[0]);
-    	System.out.println("age ="+ age[1]);
-    	System.out.println("age ="+ age[2]);
-    	System.out.println("age ="+ age[3]);
-    	System.out.println("age ="+ age[4]);
-    	System.out.println("age ="+ age[5]);
-    	System.out.println("age ="+ age[6]);
-    	System.out.println("age ="+ age[8]);
-    	System.out.println("age ="+ age[7]);
-    	int somme = age[0]+age[1]+age[2]+age[3]+age[4]+age[5]+age[6]+age[7]+age[8];
-    	System.out.println("somme ="+ somme);
-    	System.out.println("cpt = "+ cpt);
-    	
-    	return age;
-    }*/
-    /*
-    public static int NbConsultationVille (String path, String ville, String sex, String ageMin, String ageMax) throws IOException{
-    	int NbConsultationVille = 0;
-    	ArrayList<ArrayList<String>> liste = new ArrayList<ArrayList<String>>();
-    	liste = SearchIndex(path, ville);
-    	int size = liste.size(); 
-    	for (int i=0; i<size; i++){
-    		if (liste.get(i).get(1)==sex || sex.equals("")){
-    			int birth = Integer.parseInt(liste.get(i).get(2).substring(0, 4));
-        		//System.out.println("birth ="+ birth);
-        		int dossier = Integer.parseInt(liste.get(i).get(3).substring(0, 4));
-        		//System.out.println("dossier ="+ dossier);
-        		int difference = dossier-birth;
-    			if ( difference<Integer.parseInt(ageMax) && difference>Integer.parseInt(ageMin)){
-    				NbConsultationVille +=1;
-    			}
-    		}
-    	}
-    	
-    	return NbConsultationVille;
-    }*/
-    
-    public static HashMap NbConsultation (String path, String sex, int ageMin, int ageMax){
-    	HashMap NbConsultation = new HashMap();
-    	//for 
-    	
-    	return NbConsultation;
-    }
-    
-    /*public static ArrayList<String> NbAge (String path){
-    	ArrayList<String> villes = new ArrayList<String>();
-    	
-    	return villes;
-    }*/
-
+   
     public static void main(String[] args) throws Exception {
-    	//SearchIndex(args[0], args[1], "1","20");
-//    	int[] tab = nbPatientsparAge(args[0], args[1]);
-//    	int tt=0;
-//    	for(int i : tab){
-//    		tt+=i;
-//    		System.out.println(i);
-//    	}
-//    	System.out.println(tt);
-
+    	
         //generateDonutJSON(args[0]);
         //generateDonutJSON(args[0], args[1]);
     	//System.out.println(getNbPatients(args[0],args[1], "1", "20", "25"));
